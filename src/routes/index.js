24e6d820802383bb52395438e0303ae1eab9794a
AkashKumar-0 <ahmvaad@gmail.com>
2024-07-31 22:15:34 +0530

    added check token route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,19 @@
 import express from 'express'
 var router = express.Router()
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
   return res.status(200).json({
     type: 'ok',
     message: 'everything works fine',
   })
+})
+router.get('/check-token', (req, res) => {
+  const token = req.cookies.get('token')
+  if (token) {
+    return res.redirect(302, '/')
+  } else {
+    return res.redirect(302, '/signin')
+  }
 })
 
 export default router
