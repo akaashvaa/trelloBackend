774f1071467adc01458be968e9f2cb2dfb5f1698
AkashKumar-0 <ahmvaad@gmail.com>
2024-07-31 19:07:56 +0530

    fixed

--- src/app.js
+++ src/app.js
@@ -5,12 +5,12 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import dotenv from 'dotenv'
-dotenv.config()
 import indexRouter from './routes/index.js'
 import { userRouter } from './routes/user/user.routes.js'
 import { taskRouter } from './routes/task/task.routes.js'
 import { errorResponseHandler } from './utils/errorResponseHandler.js'
 
+dotenv.config()
 var app = express()
 
 app.use(
